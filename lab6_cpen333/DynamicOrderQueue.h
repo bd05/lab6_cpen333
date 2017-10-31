707c2b10bd4a334b76d79beb92f93de2234cd592
bd05 <berdao.09@gmail.com>
2017-10-31 12:31:58 -0700

    Add project files.

---
+++ lab6_cpen333/DynamicOrderQueue.h
@@ -0,0 +1,62 @@
+#ifndef LAB6_DYNAMICORDERQUEUE_H
+#define LAB6_DYNAMICORDERQUEUE_H
+
+#include "OrderQueue.h"
+#include <deque>
+#include <condition_variable>
+#include <mutex>
+
+/**
+* Dynamically-sized Queue Implementation
+*
+* Does not block when adding items
+*/
+class DynamicOrderQueue : public virtual OrderQueue {
+	std::deque<Order> buff_;
+	std::mutex mutex_;
+	std::condition_variable cv_;
+
+public:
+	/**
+	* Creates the dynamic queue
+	*/
+	DynamicOrderQueue() :
+		buff_(), mutex_(), cv_() {}
+
+	void add(const Order& order) {
+
+		//==================================================
+		// TODO: Safely add item to "queue"
+		//    - safely add to end of internal queue
+		//    - notify others of item availability
+		//==================================================
+		std::unique_lock<std::mutex> lock(mutex_);
+		//cv_.wait(lock, [&]() { return true; }); //no condition here, right?
+
+		buff_.push_back(order);
+
+		lock.unlock();
+		cv_.notify_one();
+
+	}
+
+	Order get() {
+
+		//==================================================
+		// TODO: Safely remove item from "queue"
+		//    - wait until internal queue is non-empty
+		//    - safely acquire item from internal queue
+		//==================================================
+		//block consumer if the queue is empty
+		// get first item in queue
+		std::unique_lock<std::mutex> lock(mutex_);
+		cv_.wait(lock, [&]() { return !buff_.empty(); }); //no condition here, right?
+		Order out = buff_.front();
+		buff_.pop_front();
+		lock.unlock();
+		//cv_.notify_all();
+		return out;
+	}
+};
+
+#endif //LAB6_DYNAMICORDERQUEUE_H
